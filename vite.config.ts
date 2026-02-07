41fc660186365f522440550e5e0dad3afe5ddb5b
NoticeMyCat <144901909+NoticeMyCat@users.noreply.github.com>
2026-02-07 16:44:04 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,17 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  build: {
-    copyPublicDir: true,
-  },
+  // Der Name deines GitHub-Repositorys (wichtig für GitHub Pages)
+  base: '/volley_juniors_salzburg/', 
+  
+  plugins: [react()],
+  
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  
+  build: {
+    // Muss auf true stehen, damit dein Logo aus dem /public Ordner kopiert wird
+    copyPublicDir: true, 
+  },
 });
