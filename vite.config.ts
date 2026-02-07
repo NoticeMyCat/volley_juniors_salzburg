91ab3e08300ecf02fab5759b5ec8d7b2f49dfafa
NoticeMyCat <144901909+NoticeMyCat@users.noreply.github.com>
2026-02-07 16:39:06 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'volley_juniors_salzburg', // Change this to your repo name
+  base: '/volley_juniors_salzburg/', // Change this to your repo name
   plugins: [react()],
 })
 
